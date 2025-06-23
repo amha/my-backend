974898a7ddb86f14cb01ec98b81ce413d084b5be
Amha Mogus <amha.mogus@gmail.com>
2025-06-22 20:18:26 -0400

    Added projects collection.

---
+++ src/collections/Projects.ts
@@ -0,0 +1,24 @@
+import { CollectionConfig } from 'payload/types'
+
+export const Projects: CollectionConfig = {
+  slug: 'projects',
+  admin: {
+    useAsTitle: 'title',
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'description',
+      type: 'textarea',
+    },
+    {
+      name: 'demoUrl',
+      type: 'text',
+      label: 'Demo URL',
+    },
+  ],
+}
