2c71e184ba9936012523f943a88550f496b69d6a
Amha Mogus <amha.mogus@gmail.com>
2025-06-27 21:01:34 -0400

    Updated node, payload, and config files.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,7 +3,7 @@ import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
-import { buildConfig } from 'payload/config'
+import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 
 import { Users } from './collections/Users'
@@ -22,10 +22,9 @@ export default buildConfig({
     // },
   },
   collections: [Users, Media, Projects],
-  globals: [
-    HomePage,
-  ],
+  globals: [HomePage],
   editor: lexicalEditor(),
+  secret: process.env.PAYLOAD_SECRET || '', 
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
