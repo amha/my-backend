44392dd0855647aadac9d27498b5fa7cbde6437b
Amha Mogus <amha.mogus@gmail.com>
2025-07-06 16:42:08 -0400

    Corrected the use of enviroment varibles for Payload URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
+
+const PAYLOAD_URL = process.env.PAYLOAD_PUBLIC_URL || 'http://localhost:3000'
+console.log(`[next.config.mjs] PAYLOAD_PUBLIC_URL: ${PAYLOAD_URL}`)
+
 const nextConfig = {
   reactStrictMode: true,
 
@@ -18,7 +22,7 @@ const nextConfig = {
   ],
 
   env: {
-    NEXT_PUBLIC_PAYLOAD_API_URL: process.env.PAYLOAD_PUBLIC_URL || 'http://localhost:3000',
+    NEXT_PUBLIC_PAYLOAD_API_URL: PAYLOAD_URL,
   },
 }
 
