78967aaa8d18aadb01f5b5bba4b002e4b1935758
Amha Mogus <amha.mogus@gmail.com>
2025-07-12 00:15:01 -0400

    Enabled appdir setting for NextJS 13+

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ import { withPayload } from '@payloadcms/next/withPayload'
 const nextConfig = {
   output: "standalone",
   reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
   serverExternalPackages: [
     'mongoose',
     'mongodb',
